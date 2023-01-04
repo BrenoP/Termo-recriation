3902b90a004f2c5c2ca5854df3730d1f48de3c01
Breno Peretta <peretta.breno@gmail.com>
2023-01-04 16:45:26 -0300

    api sending random word

--- pages/api/wordGeneration.ts
+++ pages/api/wordGeneration.ts
@@ -2,12 +2,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  word: string
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const words = [
+    "moeda",
+    "servo",
+    "quinto"
+  ]
+  let randomWord = words[Math.floor(Math.random() * words.length)]
+  res.status(200).json({ word: randomWord })
 }
